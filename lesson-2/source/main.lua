1859140db03d280190bc78989fa319575d74dddc
Nilay Patel <nilayp@gmail.com>
2025-07-29 10:39:39 -0700

    a few minor changes

--- lesson-2/source/main.lua
+++ lesson-2/source/main.lua
@@ -6,13 +6,14 @@ local gfx <const> = pd.graphics
 local random1 = math.random(1, 10)
 -- write an algorithm that prints the random number
 -- and then prints the statment, "less than 5" or "greater than 5"
--- depending on the value of r.
+-- depending on the value of random1.
 
-local random2 = math.random(30)
+
+
+local random2 = math.random(1,30)
 -- write an algorithm that prints the random number
 -- and then prints the statment, "less than 10" or "between 10-20" or "between 21-30"
--- depending on the value of r.
-
+-- depending on the value of random2.
 
 local odd_numbers = 1
 -- print the first 10 odd numbers
